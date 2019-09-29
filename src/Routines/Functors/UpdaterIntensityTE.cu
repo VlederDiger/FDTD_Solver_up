36d7572cdc5c6ddf0c3887a6d062fd62841bbeca
VlederDiger <55497791+VlederDiger@users.noreply.github.com>
2019-09-29 23:13:00 +0300

    Update UpdaterIntensityTE.cu

--- src/Routines/Functors/UpdaterIntensityTE.cu
+++ src/Routines/Functors/UpdaterIntensityTE.cu
@@ -10,7 +10,7 @@
 __device__
 void UpdaterIntensityTE::operator() (const int indx) {
 	#define Ex(M, N) Ex[(M) * (gridSizeY) + (N)]
-	#define Ey(M, N) Ey[(M) * (gridSizeY) + (N)]
+	#define Ey(M, N) Ey[(M) * (gridSizeY) + (N)] // gridSizeY-1?
 	const int n = indx % sizeY;
 	const int m = indx / sizeY;
 	intensity[indx] = intensity[indx] + sqrt(Ex(firstX + m*stepX, firstY + n*stepX)*Ex(firstX + m*stepX, firstY + n*stepX)+Ey(firstX + m*stepX, firstY + n*stepX)*Ey(firstX + m*stepX, firstY + n*stepX));
